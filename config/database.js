08fb85d2d01362325331f0c26188f10c1552c1ed
RR <RubenR42@icloud.com>
2020-04-10 16:30:38 -0700

    working on authorization

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const mongoose = require(“mongoose”);
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -10,6 +10,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 const db = mongoose.connection;
 
-db.on('connected', () => {
+db.once('connected', () => {
     console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
