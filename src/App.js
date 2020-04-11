c2c4219b607151114ce000dd586933ab7f32dc1b
RR <RubenR42@icloud.com>
2020-04-11 14:18:49 -0700

    updates Auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
-import SignupForm from './components/SignupForm/SignupForm';
+import SignupForm from './components/SignupForm/SignupForm'
+import userService from './utils/userService'
+import { Switch, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import LoginPage from './pages/LoginPage/LoginPage'
 
-function App() {
-  return (
-    <div className="App">
-      <SignupForm/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: userService.getUser()
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar user={this.state.user} handleLogout={this.handleLogout}/>
+        <SignupForm handleSignupOrLogin={this.handleSignupOrLogin}/>
+        <LoginPage handleSignupOrLogin={this.handleSignupOrLogin} />
+      </div>
+    );
+  }
 }
 
 export default App;
