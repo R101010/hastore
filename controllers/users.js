5c5b54d81dba1ad1cc3d1a19fd4fb3cc3ae396ba
RR <RubenR42@icloud.com>
2020-04-17 14:45:59 -0700

    fixing heroku

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ async function signup(req, res) {
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({email: req.body.email});
+    const user = User.findOne({email: req.body.email});
     if (!user) return res.status(401).json({err: 'bad credentials'});
     user.comparePassword(req.body.pw, (err, isMatch) => {
       if (isMatch) {
