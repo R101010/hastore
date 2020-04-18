cabe4c9fbc1f307c51ebc4d92f78b3b6fe07aae8
RR <RubenR42@icloud.com>
2020-04-17 23:38:23 -0700

    adds view and add notes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../../pages/App/App.css';
 import userService from '../../utils/userService';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 import LoginPage from '../../pages/LoginPage/LoginPage';
 import SignupPage from '../../pages/SignupPage/SignupPage';
@@ -22,28 +22,35 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <header className='header-footer'> The Second Opinion</header>
+      <div className="App">
+        <header className='header-footer'>
+          The Second Opinion
+          <nav>
+            <NavLink exact to='/'>VIEW NOTES</NavLink>
+            &nbsp;&nbsp;&nbsp;
+            <NavLink exact to='/add'>ADD NOTES</NavLink>
         <Switch>
           <Route exact path='/' render={() =>
             <NavBar 
-              user={this.state.user} 
-              handleLogout={this.handleLogout}
+            user={this.state.user} 
+            handleLogout={this.handleLogout}
             />
           }/>
           <Route exact path='/signup' render={({ history }) => 
             <SignupPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
+            history={history}
+            handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
           <Route exact path='/login' render={({ history }) => 
             <LoginPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
+            history={history}
+            handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
         </Switch>
+        </nav>
+        </header>
       </div>
     );
   }
