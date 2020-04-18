2541d85d2fe5b0dc5cebf2958b574f77fb586c88
RR <RubenR42@icloud.com>
2020-04-18 03:43:53 -0700

    updates NoteListPage.jsx

--- src/pages/NoteListPage/NoteListPage.jsx
+++ src/pages/NoteListPage/NoteListPage.jsx
@@ -1,2 +1,22 @@
 import React from 'react';
-import
+import './NoteListPage.css';
+import NoteListItem from '../../components/NoteListItem/NoteListItem';
+
+function NoteListPage(props) {
+    return (
+        <>
+            <h1>Note List</h1>
+            <div className='NoteListPage-grid'>
+                {props.notes.map(note => 
+                    <NoteListItem
+                        note={note}
+                        handleDeleteNote={props.handleDeleteNote}
+                        key={note._id}
+                    />  
+                )}
+            </div>
+        </>
+    );
+}
+
+export default NoteListPage;
