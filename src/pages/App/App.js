943d68f213879062dd5ea921cff725d59ae4e520
RR <RubenR42@icloud.com>
2020-04-19 19:47:14 -0700

    added pitch

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -88,6 +88,36 @@ class App extends Component {
         </Switch>
         </nav>
         </header>
+        <div style={{ display: "flex" }}>
+        <div style={{ flex: "1", fontSize: "20px", borderStyle: 'solid', fontWeight: 'bold'}}>
+
+          <p>
+            Welcome to the Hearing Aid Second Opinion. Getting a hearing aid to treat your hearing loss can be difficult and 
+            expensive. Making the wrong decision means you are left in the same place where you started.
+          </p>
+          <p>
+            I am Board Certified in Hearing Instrument Science (BC-HIS) and licensed to dispense hearing aids in the state of California (HAD). 
+            The only thing I am offering you is an honest evaluation of your hearing loss and how to treat it with hearing aids. 
+          </p>
+          <p>
+            You will receive a 90-minute consultation where I will evaluate your hearing history, audiogram results, and discuss 
+            what kind of hearing aid will help you and where you should buy it. 
+          </p>
+          <p>
+            I have personally vetted a select group of hearing aid offices that will provide you with top-level service and cutting edge technology.
+          </p>
+          <p>
+            The consultation fee is $250. 
+          </p>
+          <p>
+          When you are ready, please email us your contact information, audiogram, and any price quotes you have received. 
+            You will receive a call shortly to schedule a consultation.
+          </p>
+          <p>
+          <a href="mailto:secondopinion@hearingaidsecondopinion.com?Subject=Second%20Opinion%20needed" target="_top">secondopinion@hearingaidsecondopinion.com</a>
+          </p>
+        </div>
+        <div style={{ flex: "1", borderStyle: 'solid' }}>
         <main>
             <>
             <Route exact path='/' render={() => 
@@ -113,6 +143,8 @@ class App extends Component {
             } />
             </>
         </main>
+        </div>
+        </div>
       </div>
     );
   }
